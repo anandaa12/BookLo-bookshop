f5f4e1625cb31bc35268dfad8afb3b83ca05d251
Anand <anand.e145@gmai.com>
2023-04-17 19:54:40 +0800

    anand's commit

---
+++ js/js.js
@@ -0,0 +1,19 @@
+
+            function myfun(){
+            if(document.myform.password1.value==''){
+                alert("Нууц үгээ оруулна уу!");
+            }
+            }
+        // function myfun(){
+        //     var x1 = document.getElementById("password1").innerHTML;
+        //     var x2 = document.getElementById.password2;
+        //     if(x1 ==''){
+        //         alert("Нууц үгээ оруулна уу!");
+        //     }
+        // }
+            // sub.onclick = function(){
+            //     var x1 = document.getElementById("password1").innerHTML;
+            //     if(x1==''){
+            //         alert("Нууц үгээ оруулна уу!");
+            //     }
+            // }
